9c2bab6ee468902ea593b0409a492efbf3a8f5d2
Lucas <lucas.94wik@gmail.com>
2016-11-12 14:50:49 +0100

    REFACTOR! Switched the substring thing to a simpler solution, now an else statement in the loop will make sure there won't be any excess + signs.

--- src/main/java/AlgorithmGenerators/AdditionEquation.java
+++ src/main/java/AlgorithmGenerators/AdditionEquation.java
@@ -26,25 +26,19 @@ public class AdditionEquation {
         for(int i = 0; i < amountOfSplits; i++){
             int splitValue = rand.nextInt(equationValue) + 1;
             equationValue -= splitValue;
-            equationString += splitValue + " + ";
+            equationString += splitValue;
 
             if(equationValue <= 0){
                 break;
             }
+            else{
+                equationString += " + ";
+            }
 
         }
-        //30+20+0 passes
+        
         if(equationValue > 0){
             equationString += equationValue;
-        }
-        else{
-            //Todo: remove last + from string
-
-            int startIndex = equationString.length() - 4;
-            int endIndex = equationString.length() - 1;
-
-            String toBeReplaced = equationString.substring(startIndex + 1, endIndex);
-            equationString = equationString.replace(toBeReplaced, "").trim();
         }
 
         return equationString;
