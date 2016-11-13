f1e3830ad453fa81fa5da6be3baac0f9cca025a7
Lucas <lucas.94wik@gmail.com>
2016-11-13 10:28:26 +0100

    GREEN! generateEquation() method in DivisionEquation now properly returns a string based on input value.

--- src/main/java/AlgorithmGenerators/DivisionEquation.java
+++ src/main/java/AlgorithmGenerators/DivisionEquation.java
@@ -5,11 +5,19 @@ package AlgorithmGenerators;
  */
 public class DivisionEquation extends EquationBase {
 
+    //These are values that I would consider easy to divide with. Numbers such as 6,7,8,9 I wold consider
+    //Hard with a random number. This is mostly to test abit aswell and see how it goes.
+    //TODO: change data-structure
+    private int[] divisionValues = { 2, 3, 4, 5, 10};
+
     public DivisionEquation(int value){
         super(value);
     }
 
     public String generateEquation(){
-        return null;
+        int denominator = divisionValues[rand.nextInt(divisionValues.length)];
+        int numerator = denominator * value;
+
+        return numerator + " / " + denominator;
     }
 }
