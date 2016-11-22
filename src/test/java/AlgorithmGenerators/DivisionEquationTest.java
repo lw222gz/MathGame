18304c9d32122f60f9e2d7db8ed66a8f3fe4ccb1
Lucas <lucas.94wik@gmail.com>
2016-11-22 14:45:40 +0100

    RED! DivisionEquation.generateEquation() does not return a proper string.

--- src/test/java/AlgorithmGenerators/DivisionEquationTest.java
+++ src/test/java/AlgorithmGenerators/DivisionEquationTest.java
@@ -3,10 +3,12 @@ package AlgorithmGenerators;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Random;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Lucas on 2016-11-13.
@@ -26,6 +28,26 @@ public class DivisionEquationTest {
         String actual = sut.generateEquation();
 
         Assert.assertTrue(actual != null && actual.length() > 0);
+    }
+
+
+    @Test
+    public void shouldReturnEquationStringHundredDividedByTwo(){
+        Random randMock = Mockito.mock(Random.class);
+        sut = new DivisionEquation(50, randMock);
+
+        //This is to simulate the numerator roll,
+        //This is expected to be added by 10 because the numerator should not be lower than 10
+        when(randMock.nextInt(91)).thenReturn(90);
+
+        //This is to simulate the denominator roll
+        //This is expected to be added by 1 because a denominator should never be abel to be zero.
+        when(randMock.nextInt(10)).thenReturn(9);
+
+        String actual = sut.generateEquation();
+        String expected = "100 / 2";
+
+        Assert.assertEquals(expected, actual);
     }
 
 
